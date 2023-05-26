25f731c219d089d051765f56f53d0fec380094da
Matthew <lanrecode23@gmail.com>
2023-05-26 14:28:16 +0100

    added

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const sectionText = document.getElementById("sectionText");
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 });
+window.addEventListener("scroll", ()=> {
+    const imageTop = sectionText.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    const elementVisible = 150;
+ // Delay the appearance of the image
+
+
+    if (imageTop < windowHeight - elementVisible) {
+        sectionText.classList.add('animate__animated' , 'animate__shakeX')
+    }
+   else if (imageTop > windowHeight - elementVisible) {
+       sectionText.classList.remove('animate__animated' , 'animate__shakeX')
+   }
+});
+
